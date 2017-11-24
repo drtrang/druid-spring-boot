51817f4a69d689d72f127bd180a7bdf6a73399ad
trang <donghao.l@hotmail.com>
2017-11-24 12:20:40 +0800

    example

--- druid-spring-boot-example/druid-spring-boot-mybatis-example/src/main/java/com/github/trang/druid/example/config/SpringDataSourceConfig.java
+++ druid-spring-boot-example/druid-spring-boot-mybatis-example/src/main/java/com/github/trang/druid/example/config/SpringDataSourceConfig.java
@@ -2,17 +2,14 @@ package com.github.trang.druid.example.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-
-import static java.util.stream.Collectors.toMap;
 
 /**
  * 多数据源配置，只在 #{@code spring.profiles.active=dynamic} 时生效
@@ -24,14 +21,10 @@ import static java.util.stream.Collectors.toMap;
 @Slf4j
 public class SpringDataSourceConfig {
 
-    @Autowired
-    private Map<String, DruidDataSource> druidDataSourceMap;
-
     @Bean
     @Primary
-    public DynamicDataSource dataSource() {
-        Map<String, DataSource> dataSourceMap = druidDataSourceMap.entrySet().stream()
-                .collect(toMap(Entry::getKey, Entry::getValue));
+    public DynamicDataSource dataSource(Map<String, DruidDataSource> druidDataSourceMap) {
+        Map<String, DataSource> dataSourceMap = new HashMap<>(druidDataSourceMap);
         return new DynamicDataSource(dataSourceMap.get("master"), dataSourceMap);
     }
 
